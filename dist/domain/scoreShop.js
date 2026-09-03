aef2b2091e6d9f5100a1057882e762cc8a90c0b4
david-jss-ldn <david.jones@jssldn.co.uk>
2026-09-03 09:11:18 +0100

    Back to minimum install

--- dist/domain/scoreShop.js
+++
@@ -1,2 +0,0 @@
-import { shopCapabilityDefaults } from '../config/shopCapabilityDefaults.js';
-export function scoreShopForCategory(shop, category) { return Math.max(0, ...shop.categories.map(c => shopCapabilityDefaults[c][category] ?? 0)); }
